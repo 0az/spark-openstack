9f5a7fac7a253cbcf498daa0da6ff94a7010bdef
afzhou <afzhou@ucsd.edu>
2021-11-20 23:22:20 -0800

    Fix ansible

--- scripts/create.sh
+++ scripts/create.sh
@@ -3,13 +3,20 @@
 scripts="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 extra_usage() {
-	echo 'To specify an OpenStack SSH key, set the KEY environment variable.'
+	echo \
+		'To specify the path to the OpenStack administrative SSH key,' \
+		'set the ADMIN_KEY environment variable.'
 }
 
 source "$scripts/_common.sh"
 
+extra_args=()
+if test -z "$ADMIN_KEY"; then
+	args+=(-i "$ADMIN_KEY")
+fi
+
 # shellcheck disable=SC2087
-exec ssh "$@" /bin/bash <<EOF
+exec ssh "${extra_args[@]}" "$@" /bin/bash <<EOF
 set -euo pipefail
 
 cd ~/spark-openstack >/dev/null || exit 2
@@ -24,29 +31,27 @@ fi
 
 source .venv/bin/activate
 
-echo ./spark-openstack \
-	--create \
-	--deploy-spark \
-	-k "$KEY" \
-	-s 1 \
-	-n flat-lan-1-net \
-	-t m1.xlarge \
-	-m m1.xlarge \
-	-a focal-server-cloudimg-amd64  \
-	--spark-version 2.4.8 \
-	launch test-project
+args=(
+	./spark-openstack
+		--create
+)
 
-test -z "$DRY_RUN" || exit 0
+test -z '$NO_DEPLOY_SPARK' && args+=(--deploy-spark)
+test -z '$NO_DEPLOY_JUPYTER' && args+=(--deploy-jupyter)
 
-./spark-openstack \
-	--create \
-	--deploy-spark \
-	-k "$KEY" \
-	-s 1 \
-	-n flat-lan-1-net \
-	-t m1.xlarge \
-	-m m1.xlarge \
-	-a focal-server-cloudimg-amd64  \
-	--spark-version 2.4.8 \
-	launch test-project
+args+=(
+		-k admin
+		-s 1
+		-n flat-lan-1-net
+		-t m1.xlarge
+		-m m1.xlarge
+		-a focal-server-cloudimg-amd64 
+		--spark-version 2.4.8
+		launch test-project
+)
+echo "\${args[@]}"
+
+test -z '$DRY_RUN' || exit 0
+
+"\${args[@]}"
 EOF
