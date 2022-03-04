5cd3285b6c8ba897d558947d99e78807e407b1c4
afzhou <afzhou@ucsd.edu>
2022-03-04 12:17:53 -0800

    Update Ansible filters

--- ansible/filter_plugins/filters.py
+++ ansible/filter_plugins/filters.py
@@ -1,4 +1,3 @@
-import json
 from ast import literal_eval
 
 
@@ -11,21 +10,45 @@ def _try_literal_eval(s):
         return s
 
 
-def parse_list(input_) -> list[str]:
-    if not input_:
+def parse_list(s) -> list[str]:
+    if isinstance(s, list):
+        return s
+
+    if not s:
         return []
 
-    input_ = input_.strip()
-    if not input_:
+    s = s.strip()
+    if not s:
         return []
 
     try:
-        return literal_eval(input_)
+        return literal_eval(s)
     except ValueError:
-        l = [_try_literal_eval(s) for s in input_.split(',')]
+        l = [_try_literal_eval(s) for s in s.split(',')]
         return l
+
+
+def casefold(s: str) -> str:
+    return s.casefold()
+
+
+def intersects_with(left, right) -> bool:
+    if left is None:
+        left = ()
+    if right is None:
+        right = ()
+    return bool(set(left) & set(right))
+
+
+def disjoint_from(left, right) -> bool:
+    return not intersects_with(left, right)
 
 
 class FilterModule:
     def filters(self):
-        return {'parse_list': parse_list}
+        return {
+            'parse_list': parse_list,
+            'casefold': casefold,
+            'intersects_with': intersects_with,
+            'disjoint_from': disjoint_from,
+        }
